3edc9f62306e67b371876c401e2e1c6f33dad811
Saan <146492966+sajohna@users.noreply.github.com>
2026-09-12 13:00:06 -0400

    Initial commit

---
+++ src/test/java/nl/tudelft/jpacman/OnboardingTest.java
@@ -0,0 +1,11 @@
+package nl.tudelft.jpacman;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class OnboardingTest {
+    @Test
+    void arithmeticSanity() {
+        assertEquals(3, 1 + 1 + 1);
+    }
+}
